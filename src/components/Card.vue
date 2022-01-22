83d7f00f1823e13605ee7d96af546e7690a9da60
Khari <khariwoods3@gmail.com>
2022-01-21 22:54:57 -0400

    Added styling to education section

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="card">
+    <div class="card-body">
+        <h3 class="card-title">{{title}}</h3>
+        <h6>{{subtitle}}</h6>
+        <p class="card-text">{{description}}</p>
+        <p class="card-text">{{summary}}</p>
+    </div>
+</div>    
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        title: String,
+        subtitle: String,
+        description: String,
+        summary: String,
+    }
+    
+}
+</script>
+
+
+<style scoped>
+.card{
+    margin: 100px auto;
+    width: 80%;
+    border-radius: 10px;
+    opacity: 0.8;
+}
+
+</style>
--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,18 +1,37 @@
 <template>
 <div id="education-background">
-    <Overlay backgroundColor="rgba(255, 0, 0,0.5)"/>
-    <Header title="Education"/>
+    <Overlay backgroundColor="rgba(112, 190, 81,0.5)"/>
+    <div id="education-header-style">
+        <Header title="Education"/>
+    </div>
+
+    <Card 
+    title="College Of The Bahamas"
+    subtitle="August 2012 - April 2015"
+    description="Associates Degree of Architecture"
+    summary="Here I studied architecture and learned about design"
+    />
+
+    <Card 
+    title="Holland College"
+    subtitle="August 2019 - May 2021"
+    description="Computer information systems"
+    summary="Here I learned how to design websites"
+    />
+    
 </div>  
 </template>
 
 <script>
 import Header from "../components/Header";
 import Overlay from "../components/Overlay.vue"
+import Card from "../components/Card.vue"
 export default {
     name: 'Education',
      components:{
          Header,
          Overlay,
+         Card
      }
 }
 </script>
@@ -20,11 +39,19 @@ export default {
 
 <style scoped>
 #education-background{
-    /* background-image: url('../assets/') */
-    /* background: rgba(8, 178, 227,.5); */
     position: relative;
     height: 100%;
     width: 100%;
+    background-image: url('../assets/images/1_educationbackground(1).jpg');
+    background-size: cover;
+    background-position: center;
+    
+}
+
+#education-header-style{
+    margin: 0 auto;
+    text-align: center;
+    padding-top: 40px;
 }
 
 </style>
