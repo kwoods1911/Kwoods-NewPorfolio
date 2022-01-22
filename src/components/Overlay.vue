e45fb609c49f5d3e941dbae460fcb66ff0d0478c
Khari <khariwoods3@gmail.com>
2022-01-21 21:56:41 -0400

    Added Overlay component

---
+++ src/components/Overlay.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="overlay" :style="{backgroundColor: color}"></div>
+</template>
+
+<script>
+export default {
+    name: 'Overlay',
+    props: {
+        backgroundColor:String
+    },
+    data(){
+        return{
+            color: ''
+        }
+    },
+    mounted(){
+        this.color = this.backgroundColor;
+    }
+}
+</script>
+
+<style scoped>
+.overlay {
+  position: absolute;
+  z-index: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+</style>
--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,3 +1,30 @@
 <template>
-    <h1>Education</h1>
+<div id="education-background">
+    <Overlay backgroundColor="rgba(255, 0, 0,0.5)"/>
+    <Header title="Education"/>
+</div>  
 </template>
+
+<script>
+import Header from "../components/Header";
+import Overlay from "../components/Overlay.vue"
+export default {
+    name: 'Education',
+     components:{
+         Header,
+         Overlay,
+     }
+}
+</script>
+
+
+<style scoped>
+#education-background{
+    /* background-image: url('../assets/') */
+    /* background: rgba(8, 178, 227,.5); */
+    position: relative;
+    height: 100%;
+    width: 100%;
+}
+
+</style>
